a41ed11c8891ea5661631d593ab4c10d0579820b
Ruben Lopez <rubenlopsol86@gmail.com>
2023-02-17 18:08:52 +0100

    start model and views

--- buenobonitobarato/models/User.model.js
+++ buenobonitobarato/models/User.model.js
@@ -3,18 +3,9 @@ const { Schema, model } = require("mongoose");
 // TODO: Please make sure you edit the User model to whatever makes sense in this case
 const userSchema = new Schema(
   {
-    username: {
-      type: String,
-      required: false,
-      unique: true,
-      trim: true,
-    },
-    email: {
+    name: {
       type: String,
       required: true,
-      unique: true,
-      trim: true,
-      lowercase: true,
     },
     password: {
       type: String,
